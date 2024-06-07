7a98dfc7ff97103391d3c6a0c38cb1701e8a6992
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-06-07 16:08:57 +0530

    decrease font size in the left pannel

--- components/left-panel.tsx
+++ components/left-panel.tsx
@@ -12,7 +12,7 @@ const LeftPanel = () => {
     >
       {/* Top Container */}
       <div>
-        <div>
+        <div >
           <Image
             priority
             loading="eager"
@@ -26,32 +26,33 @@ const LeftPanel = () => {
         </div>
 
         {/* Text Container */}
-        <div className="mt-6">
-          <div className="text-xl font-medium text-primary">
+        <div className="mt-1">
+          <div className="text-xl font-bold text-primary">
             {siteConfig.title}
           </div>
-          <h1 className="mt-2 text-4xl font-bold">{siteConfig.creator}</h1>
-          <p className="text-2xl font-light text-neutral-500">
+          <h1 className="mt-1 text-4xl font-bold">{siteConfig.creator}</h1>
+          <p className="text-l mt-1 font-bold text-neutral-500 ">
             {siteConfig.bio}
           </p>
         </div>
         {/* Buttons Container */}
-        {/* <div className="flex items-center gap-3 mt-6">
-          <a
-            href={`${siteConfig.locationLink}`}
-            className="flex items-center w-full gap-2 px-4 py-2 text-sm font-medium border rounded-md border-neutral-100 dark:border-neutral-800"
-          >
-            <MapPin size="14" />
-            {siteConfig.location}
-          </a>
-          <a
-            href={`mailto:${siteConfig.email}`}
-            className="flex items-center w-full gap-2 px-4 py-2 text-sm font-medium border rounded-md border-neutral-100 dark:border-neutral-800"
-          >
-            <Mail size="14" />
-            Contact Me
-          </a>
-        </div> */}
+        {/*  <div className="hidden lg-block">
+          <div className="flex items-center gap-3 mt-6">
+            <a
+              href={``}
+              className="flex items-center w-full gap-2 px-4 py-2 text-sm font-medium border rounded-md border-neutral-100 dark:border-neutral-800"
+            >
+              {siteConfig.location}
+            </a>
+            <a
+              href={`mailto:${siteConfig.email}`}
+              className="flex items-center w-full gap-2 px-4 py-2 text-sm font-medium border rounded-md border-neutral-100 dark:border-neutral-800 "
+            >
+              <Mail size="14" />
+              Contact Me
+            </a>
+          </div>"}
+        </div>*/}
         {/* Footer */}
         <div className="hidden mt-6 xl:flex">
           <Footer />
